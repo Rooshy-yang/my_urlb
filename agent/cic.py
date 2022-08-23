a1eccff474e0974f804c55539fb2fa9e37cd1fbf
yangrushuai <54839995+Rooshy-yang@users.noreply.github.com>
2022-08-23 09:44:28 +0800

    update format

--- agent/cic.py
+++ agent/cic.py
@@ -12,41 +12,39 @@ import utils
 from agent.ddpg import DDPGAgent
 
 
-
-
 class CIC(nn.Module):
     def __init__(self, obs_dim, skill_dim, hidden_dim, project_skill):
         super().__init__()
         self.obs_dim = obs_dim
         self.skill_dim = skill_dim
 
-        self.state_net = nn.Sequential(nn.Linear(self.obs_dim, hidden_dim), nn.ReLU(), 
-                                        nn.Linear(hidden_dim, hidden_dim), nn.ReLU(), 
-                                        nn.Linear(hidden_dim, self.skill_dim))
+        self.state_net = nn.Sequential(nn.Linear(self.obs_dim, hidden_dim), nn.ReLU(),
+                                       nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+                                       nn.Linear(hidden_dim, self.skill_dim))
 
-        self.next_state_net = nn.Sequential(nn.Linear(self.obs_dim, hidden_dim), nn.ReLU(), 
-                                        nn.Linear(hidden_dim, hidden_dim), nn.ReLU(), 
-                                        nn.Linear(hidden_dim, self.skill_dim))
+        self.next_state_net = nn.Sequential(nn.Linear(self.obs_dim, hidden_dim), nn.ReLU(),
+                                            nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+                                            nn.Linear(hidden_dim, self.skill_dim))
 
-        self.pred_net = nn.Sequential(nn.Linear(2 * self.skill_dim, hidden_dim), nn.ReLU(), 
-                                        nn.Linear(hidden_dim, hidden_dim), nn.ReLU(), 
-                                        nn.Linear(hidden_dim, self.skill_dim))
+        self.pred_net = nn.Sequential(nn.Linear(2 * self.skill_dim, hidden_dim), nn.ReLU(),
+                                      nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+                                      nn.Linear(hidden_dim, self.skill_dim))
 
         if project_skill:
             self.skill_net = nn.Sequential(nn.Linear(self.skill_dim, hidden_dim), nn.ReLU(),
-                                            nn.Linear(hidden_dim, hidden_dim), nn.ReLU(), 
-                                            nn.Linear(hidden_dim, self.skill_dim))
+                                           nn.Linear(hidden_dim, hidden_dim), nn.ReLU(),
+                                           nn.Linear(hidden_dim, self.skill_dim))
         else:
-            self.skill_net = nn.Identity()  
-   
+            self.skill_net = nn.Identity()
+
         self.apply(utils.weight_init)
 
-    def forward(self,state,next_state,skill):
-        assert len(state.sizes()) == len(next_state.sizes())
+    def forward(self, state, next_state, skill):
+        assert len(state.size()) == len(next_state.size())
         state = self.state_net(state)
         next_state = self.state_net(next_state)
         query = self.skill_net(skill)
-        key = self.pred_net(torch.cat([state,next_state],1))
+        key = self.pred_net(torch.cat([state, next_state], 1))
         return query, key
 
 
@@ -60,10 +58,11 @@ class RMS(object):
         self.n = epsilon
 
     def __call__(self, x):
-        bs = x.sizes(0)
+        bs = x.size(0)
         delta = torch.mean(x, dim=0) - self.M
         new_M = self.M + delta * bs / (self.n + bs)
-        new_S = (self.S * self.n + torch.var(x, dim=0) * bs + (delta**2) * self.n * bs / (self.n + bs)) / (self.n + bs)
+        new_S = (self.S * self.n + torch.var(x, dim=0) * bs + (delta ** 2) * self.n * bs / (self.n + bs)) / (
+                    self.n + bs)
 
         self.M = new_M
         self.S = new_S
@@ -71,19 +70,20 @@ class RMS(object):
 
         return self.M, self.S
 
+
 class APTArgs:
-    def __init__(self,knn_k=16,knn_avg=True, rms=True,knn_clip=0.0005,):
-        self.knn_k = knn_k 
-        self.knn_avg = knn_avg 
-        self.rms = rms 
+    def __init__(self, knn_k=16, knn_avg=True, rms=True, knn_clip=0.0005, ):
+        self.knn_k = knn_k
+        self.knn_avg = knn_avg
+        self.rms = rms
         self.knn_clip = knn_clip
+
 
 rms = RMS()
 
 
 def compute_apt_reward(source, target, args):
-
-    b1, b2 = source.sizes(0), target.sizes(0)
+    b1, b2 = source.size(0), target.size(0)
     # (b1, 1, c) - (1, b2, c) -> (b1, 1, c) - (1, b2, c) -> (b1, b2, c) -> (b1, b2)
     sim_matrix = torch.norm(source[:, None, :].view(b1, 1, -1) - target[None, :, :].view(1, b2, -1), dim=-1, p=2)
     reward, _ = sim_matrix.topk(args.knn_k, dim=1, largest=False, sorted=True)  # (b1, k)
@@ -109,8 +109,8 @@ def compute_apt_reward(source, target, args):
 
 class CICAgent(DDPGAgent):
     # Contrastive Intrinsic Control (CIC)
-    def __init__(self, update_skill_every_step, skill_dim, scale, 
-                    project_skill, rew_type, update_rep, temp, **kwargs):
+    def __init__(self, update_skill_every_step, skill_dim, scale,
+                 project_skill, rew_type, update_rep, temp, **kwargs):
         self.temp = temp
         self.skill_dim = skill_dim
         self.update_skill_every_step = update_skill_every_step
@@ -120,16 +120,15 @@ class CICAgent(DDPGAgent):
         self.update_rep = update_rep
         kwargs["meta_dim"] = self.skill_dim
         # create actor and critic
-        
 
         super().__init__(**kwargs)
         # create cic first
         self.cic = CIC(self.obs_dim - skill_dim, skill_dim,
-                           kwargs['hidden_dim'], project_skill).to(kwargs['device'])
+                       kwargs['hidden_dim'], project_skill).to(kwargs['device'])
 
         # optimizers
         self.cic_optimizer = torch.optim.Adam(self.cic.parameters(),
-                                                lr=self.lr)
+                                              lr=self.lr)
 
         self.cic.train()
 
@@ -142,7 +141,7 @@ class CICAgent(DDPGAgent):
             # procedures described in the CIC paper)
             skill = np.ones(self.skill_dim).astype(np.float32) * 0.5
         else:
-            skill = np.random.uniform(0,1,self.skill_dim).astype(np.float32)
+            skill = np.random.uniform(0, 1, self.skill_dim).astype(np.float32)
         meta = OrderedDict()
         meta['skill'] = skill
         return meta
@@ -152,20 +151,20 @@ class CICAgent(DDPGAgent):
             return self.init_meta()
         return meta
 
-    def compute_cpc_loss(self,obs,next_obs,skill):
+    def compute_cpc_loss(self, obs, next_obs, skill):
         temperature = self.temp
         eps = 1e-6
-        query, key = self.cic.forward(obs,next_obs,skill)
+        query, key = self.cic.forward(obs, next_obs, skill)
         query = F.normalize(query, dim=1)
         key = F.normalize(key, dim=1)
-        cov = torch.mm(query,key.T) # (b,b)
-        sim = torch.exp(cov / temperature) 
-        neg = sim.sum(dim=-1) # (b,)
-        row_sub = torch.Tensor(neg.shape).fill_(math.e**(1 / temperature)).to(neg.device)
+        cov = torch.mm(query, key.T)  # (b,b)
+        sim = torch.exp(cov / temperature)
+        neg = sim.sum(dim=-1)  # (b,)
+        row_sub = torch.Tensor(neg.shape).fill_(math.e ** (1 / temperature)).to(neg.device)
         neg = torch.clamp(neg - row_sub, min=eps)  # clamp for numerical stability
 
-        pos = torch.exp(torch.sum(query * key, dim=-1) / temperature) #(b,)
-        loss = -torch.log(pos / (neg + eps)) #(b,)
+        pos = torch.exp(torch.sum(query * key, dim=-1) / temperature)  # (b,)
+        loss = -torch.log(pos / (neg + eps))  # (b,)
         return loss, cov / temperature
 
     def update_cic(self, obs, skill, next_obs, step):
@@ -184,10 +183,10 @@ class CICAgent(DDPGAgent):
         return metrics
 
     def compute_intr_reward(self, obs, skill, next_obs, step):
-        
+
         with torch.no_grad():
             loss, logits = self.compute_cpc_loss(obs, next_obs, skill)
-      
+
         reward = loss
         reward = reward.clone().detach().unsqueeze(-1)
 
@@ -198,8 +197,8 @@ class CICAgent(DDPGAgent):
         args = APTArgs()
         source = self.cic.state_net(obs)
         target = self.cic.state_net(next_obs)
-        reward = compute_apt_reward(source, target, args) # (b,)
-        return reward.unsqueeze(-1) # (b,1)
+        reward = compute_apt_reward(source, target, args)  # (b,)
+        return reward.unsqueeze(-1)  # (b,1)
 
     def update(self, replay_iter, step):
         metrics = dict()
@@ -214,14 +213,14 @@ class CICAgent(DDPGAgent):
 
         with torch.no_grad():
             obs = self.aug_and_encode(obs)
-        
+
             next_obs = self.aug_and_encode(next_obs)
 
         if self.reward_free:
             if self.update_rep:
                 metrics.update(self.update_cic(obs, skill, next_obs, step))
 
-            intr_reward = self.compute_apt_reward(next_obs,next_obs)
+            intr_reward = self.compute_apt_reward(next_obs, next_obs)
 
             reward = intr_reward
         else:
